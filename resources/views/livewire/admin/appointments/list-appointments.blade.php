bf5e335c6765ee2c3ec1653001040d3cfe551ef7
Mathew Mbugua <mbuguam443@gmail.com>
2025-10-03 19:55:06 +0300

    sato finish

--- resources/views/livewire/admin/appointments/list-appointments.blade.php
+++ resources/views/livewire/admin/appointments/list-appointments.blade.php
@@ -1,27 +1,5 @@
 <div>
-    <div wire:loading.delay>
-        <div style="display:flex;
-                justify-content:center;
-                align-items:center;
-                background-color:black;
-                position:fixed;
-                top:0px;left:0px;
-                z-index:9999;
-                width:100%;
-                height:100%;
-                opacity:.75;">
-            <div class="la-ball-spin la-2x">
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-            </div>
-        </div>
-    </div>
+    <x-loading-indicator/>
     <!-- Content Header (Page header) -->
     <div class="content-header">
         <div class="container-fluid">
